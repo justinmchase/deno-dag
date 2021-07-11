7f4ca2eab020b793db622e6e2b0d12d573260b4e
Justin Chase <justin.m.chase@gmail.com>
2021-07-11 15:09:55 -0500

    Adds code comments

--- mod.ts
+++ mod.ts
@@ -12,25 +12,31 @@ export class GraphError extends Error {
   }
 }
 
-// Khan's algorithm for topographical sorting
-// https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm
-//
-// L ← Empty list that will contain the sorted elements
-// S ← Set of all nodes with no incoming edges
-// while S is non-empty do
-//     remove a node n from S
-//     add n to tail of L
-//     for each node m with an edge e from n to m do
-//         remove edge e from the graph
-//         if m has no other incoming edges then
-//             insert m into S
-// if graph has edges then
-//     return error (graph has at least one cycle)
-// else
-//     return L (a topologically sorted order)
-//
-// note: 'no incoming edges' means no dependencies on.
-//       This means that edges is an index of what other targets depend on it.
+/**
+ * Khan's algorithm for topographical sorting
+ * https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm
+ * 
+ * L ← Empty list that will contain the sorted elements
+ * S ← Set of all nodes with no incoming edges
+ * while S is non-empty do
+ *   remove a node n from S
+ *   add n to tail of L
+ *   for each node m with an edge e from n to m do
+ *     remove edge e from the graph
+ *     if m has no other incoming edges then
+ *       insert m into S
+ * if graph has edges then
+ *   return error (graph has at least one cycle)
+ * else
+ * return L (a topologically sorted order)
+ * 
+ * note:
+ * 'no incoming edges' means no dependencies on.
+ * This means that edges is an index of what other targets depend on it.
+ * 
+ * @param {ITarget[]} targets
+ * @returns {ITarget[]} The targets sorted
+ */
 export function topographicalSort(targets: ITarget[]): ITarget[] {
   const index: Record<string, ITarget> = {};
   const edges: Record<string, string[]> = {};
